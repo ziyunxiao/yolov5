63ee0354715b5e5a9d2a10de993a617f7f2bf822
robin <ziyunxiao@gmail.com>
2021-01-19 07:14:13 -0700

    update test1

--- test1.py
+++ test1.py
@@ -1,8 +1,19 @@
-python detect.py --source 0  # webcam
-                            file.jpg  # image 
-                            file.mp4  # video
-                            path/  # directory
-                            path/*.jpg  # glob
-                            rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa  # rtsp stream
-                            rtmp://192.168.1.105/live/test  # rtmp stream
-                            http://112.50.243.8/PLTV/88888888/224/3221225900/1.m3u8  # http stream
+import torch
+from PIL import Image
+
+# Model
+model = torch.hub.load(
+    "ultralytics/yolov5",
+    "yolov5s",
+    pretrained=True,
+)
+
+# Images
+img1 = Image.open("data/images/zidane.jpg")
+img2 = Image.open("data/images/bus.jpg")
+imgs = [img1, img2]  # batched list of images
+
+# Inference
+result = model(imgs)
+
+print(result)
